9ee8e90365a12d25d19bd28040f7e9afbc2628a9
Budanidze <161483945+Budanidze@users.noreply.github.com>
2024-05-19 14:49:45 +0300

    Add files via upload

---
+++ db/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS Emails(
+    ID SERIAL PRIMARY KEY,
+    Email VARCHAR (100) NOT NULL
+);
+
+INSERT INTO Emails (Email)
+VALUES  ('bot@mail.ru'),
+	('bob@gmail.com');
+
+CREATE TABLE IF NOT EXISTS Phones(
+    ID SERIAL PRIMARY KEY,
+    Phone VARCHAR (100) NOT NULL
+);
+
+INSERT INTO Phones (Phone)
+VALUES  ('+7(800)5553535'),
+	('89845551553');
